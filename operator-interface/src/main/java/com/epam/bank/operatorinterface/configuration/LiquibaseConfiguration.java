dbbc1ffbf61d625ced7d5584c5e73a4c05f4caab
Evgenii Ponukhov <evgenii_ponukhov@epam.com>
2021-12-28 22:29:40 +0300

    fixed check style errors

--- operator-interface/src/main/java/com/epam/bank/operatorinterface/configuration/LiquibaseConfiguration.java
+++ operator-interface/src/main/java/com/epam/bank/operatorinterface/configuration/LiquibaseConfiguration.java
@@ -1,15 +1,15 @@
 package com.epam.bank.operatorinterface.configuration;
 
+import javax.sql.DataSource;
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import javax.sql.DataSource;
 
 @Configuration
 public class LiquibaseConfiguration {
 
     @Bean
-    public SpringLiquibase liquibase(DataSource dataSource)  {
+    public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
 
         liquibase.setDataSource(dataSource);
